cdb560d53cadd99f0ba13ef58c5d3ab129f75f55
Angelina <svlvangel@gmail.com>
2019-04-19 17:50:39 +0300

    первый комит

--- Project/script.js
+++ Project/script.js
@@ -1,2 +1,18 @@
+let money = prompt("Ваш бюджет на месяц?", '');
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
-alert("Hello World");
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {"ответ на первый вопрос" : "ответ на второй вопрос"},
+    optionalExpenses: {},
+    income: {},
+    savings: false
+
+};
+let q1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    q2 = prompt("Во сколько обойдётся?", '');
+
+appData.expenses.q1=q2;
+
+alert(appData.budget / 30);
