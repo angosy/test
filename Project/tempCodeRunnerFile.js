bb8a1825fe9621a64c2d240dc7eb90b7003b2b70
Angelina <svlvangel@gmail.com>
2019-04-19 22:29:01 +0300

    3 задание

--- Project/tempCodeRunnerFile.js
+++ Project/tempCodeRunnerFile.js
@@ -1,4 +1,4 @@
-let money = +prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", '');
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
@@ -12,15 +12,14 @@ let appData = {
 };
 for ( i = 0; i < 2; i++){
     let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = prompt("Во сколько обойдётся?", '');
+    b = prompt("Во сколько обойдётся?", '');
 
     if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null
     && a != '' && b != '' && a.length < 50 ) {
         console.log("done");
         appData.expenses[a] = b;
     } else {
-        console.log("bad result"),
-        i--;
+
     }
 };
 
